5b2a9fc44a9b8b92be6d98e1614f92aab2292bd0
Dinusha <dmchandrasinghe@gmail.com>
2021-06-20 21:02:22 +0530

    Login and Logout

--- Aruba/ArubaCX.py
+++ Aruba/ArubaCX.py
@@ -11,5 +11,3 @@ print(f"Login code from Switch: {login.status_code}")
 print(f"This is Cookie: {login.cookies}")
 logout = session.post("https://192.168.11.254/rest/v1/logout")
 print(f"Logout Code from Switch:{logout.status_code}")
-
-
