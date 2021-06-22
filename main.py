43d063d61f0bc73e8d00175c165b503cee9fba4c
Dinusha <dmchandrasinghe@gmail.com>
2021-06-22 18:52:04 +0530

    introduce data process file for analyze log file in to separate db

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from Aruba.ArubaCX import *
-from Control.dbconnect import *
-from threading import Thread
 
 ip_add = "192.168.11.254"
-
+# get logs from Switch and Store in to DB
 ArubaCX_Switch.get_logs(ip_add)
